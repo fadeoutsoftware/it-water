d279065405d95a289b4868b1b3d2ecf396c0fe0c
Marco Menapace <m.menapace@fadeout.it>
2024-12-04 17:53:42 +0100

    Working setup need to be tested with example data

---
+++ hmc-runner/launcher.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+# Command to be launched within docker
+# /app/library/libs_system/hmc/HMC_Model_V3_$RUN.x
+#/app/library/libs_system/hmc
+
+/app/library/libs_system/hmc/HMC_Model_V3_\$RUN.x
