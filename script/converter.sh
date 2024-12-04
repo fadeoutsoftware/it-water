9eaf76424a7dee64fbb75c4691de1d15422d4d47
Alberto Tasso <a.tasso@fadeout.it>
2024-12-04 11:51:32 +0000

    refctoring of converter container

--- script/converter.sh
+++ script/converter.sh
@@ -1,9 +1,6 @@
 docker run \
--it \
 --name converter \
 --mount type=bind,source=/home/taxalb/data,target=/data \
 docker.io/it-water/converter:dev
 
-# docker cp hmc_tool_processing_sourcenc2nc_converter_s3m_fotest.json converter:/app/entrypoint
-
-# docker exec -it converter bash
+docker rm converter
