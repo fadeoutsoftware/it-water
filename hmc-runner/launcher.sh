1e3282a2fe6b0d06914d6616484c9e562c21bd14
Marco Menapace <m.menapace@fadeout.it>
2025-02-06 10:15:23 +0100

    Working configuration, missing data ad mount

--- hmc-runner/launcher.sh
+++ hmc-runner/launcher.sh
@@ -4,4 +4,6 @@
 # /app/library/libs_system/hmc/HMC_Model_V3_$RUN.x
 #/app/library/libs_system/hmc
 
+source $PYTHON_ENV_FILE
+
 /app/library/libs_system/hmc/HMC_Model_V3_\$RUN.x /app/mnt_in/namelist/marche.info.txt
