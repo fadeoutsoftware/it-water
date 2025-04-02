5565f1fbb9bf29fc95afabdf78ba3f631e887b63
Marco Menapace <m.menapace@fadeout.it>
2025-04-02 10:32:28 +0200

    Splitted venv setup and launcher to optimize execution time

--- converter/launcher.sh
+++ converter/launcher.sh
@@ -1,16 +1,10 @@
 #!/bin/bash -e
-
-
 cd $CONDA_PATH 
 cd bin 
 source activate shybox_base_libraries
 cd /app/shybox/workflow/converter
-
 export PYTHONPATH="${PYTHONPATH}:/app/shybox"
 
-pip install tabulate
-pip install rioxarray
-pip install pyresample
-pip install repurpose
+
 
 python /app/shybox/workflow/converter/app_converter_workflow_s3m_base_main.py -settings /app/shybox/workflow/converter/app_converter_workflow_s3m_base.json -time "2025-03-26 12:00"
