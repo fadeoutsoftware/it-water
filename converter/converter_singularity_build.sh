944b15c0f9284792bf55fd409cbbc55141623dcf
Marco Menapace <m.menapace@fadeout.it>
2025-04-02 16:27:20 +0200

    Fixed potential error occurring for multiple docker tags before sif convertion

--- converter/converter_singularity_build.sh
+++ converter/converter_singularity_build.sh
@@ -5,6 +5,6 @@ rm converter.tar
 # Build docker image
 docker build --progress=plain -t it-water/converter:dev .
 # Save image in tar format 
-docker save it-water/converter -o converter.tar
+docker save it-water/converter:dev -o converter.tar
 # Convert to singularity 
 singularity build converter.sif docker-archive://converter.tar
--- s3m-runner/s3m_singularity_build.sh
+++ s3m-runner/s3m_singularity_build.sh
@@ -5,6 +5,6 @@ rm s3m.tar
 # Build docker image
 docker build --progress=plain -t it-water/s3m-runner:dev .
 # Save image in tar format 
-docker save it-water/s3m-runner -o s3m.tar
+docker save it-water/s3m-runner:dev -o s3m.tar
 # Convert to singularity 
 singularity build s3m.sif docker-archive://s3m.tar
