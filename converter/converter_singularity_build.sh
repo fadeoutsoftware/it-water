18b9d2a348545021d570347bc18ce2309d698182
Marco Menapace <m.menapace@fadeout.it>
2025-04-08 17:19:53 +0200

    Batch convertion of crlf with dos 2 unix

--- converter/converter_singularity_build.sh
+++ converter/converter_singularity_build.sh
@@ -2,6 +2,11 @@
 #cleanup previous versions 
 rm converter.sif
 rm converter.tar
+## DOS2UNIX launcher.sh
+dos2unix ./launcher.sh
+dos2unix ./app_converter_workflow_s3m_base.json
+dos2unix ./app_converter_workflow_s3m_base_main.py
+
 # Build docker image
 docker build --progress=plain -t it-water/converter:dev .
 # Save image in tar format 
--- s3m-runner/s3m_singularity_build.sh
+++ s3m-runner/s3m_singularity_build.sh
@@ -2,6 +2,11 @@
 #cleanup previous versions 
 rm s3m.sif
 rm s3m.tar
+
+## DOS2UNIX launcher.sh
+dos2unix ./launcher.sh
+dos2unix ./app_runner_workflow_s3m_base.json
+
 # Build docker image
 docker build --progress=plain -t it-water/s3m-runner:dev .
 # Save image in tar format 
