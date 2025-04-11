fb31484a21d4e1a6200d2f2ad4898d31f9e1b29d
Marco Menapace <m.menapace@fadeout.it>
2025-04-11 15:04:44 +0200

    dos2unix in local build

--- converter/build.sh
+++ converter/build.sh
@@ -1,1 +1,7 @@
-docker build  --progress=plain -t it-water/converter:dev . 
+
+## DOS2UNIX launcher.sh
+dos2unix ./launcher.sh
+dos2unix ./app_converter_workflow_s3m_base.json
+dos2unix ./app_converter_workflow_s3m_base_main.py
+
+docker build --no-cache --progress=plain -t it-water/converter:dev . 
