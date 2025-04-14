194d076ca3a30f8e7e11b4c3829f04dd3809c6e3
Marco Menapace <m.menapace@fadeout.it>
2025-04-14 18:04:39 +0200

    Fixed static and Json reference

--- s3m-runner/launcher.sh
+++ s3m-runner/launcher.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -e
-source $PYTHON_ENV_FILE
-
+cd $CONDA_PATH 
+cd bin 
+source activate shybox_base_libraries
+cd /app/shybox/workflow/runner
 export PYTHONPATH="${PYTHONPATH}:/app/shybox"
+echo -----------------------------------------------------
+echo JSON PATH : 
+echo $JSON_PATH
+echo -----------------------------------------------------
 
-path_json=$1
-echo "path_json = " ${path_json}
-
-python /app/shybox/workflow/runner/app_runner_workflow_s3m_base_main.py -settings ${path_json}
+python /app/shybox/workflow/runner/app_runner_workflow_s3m_base_main.py -settings $JSON_PATH
