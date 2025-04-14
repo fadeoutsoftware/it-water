4d6a36533f9b4a10425d2e953a606a4d4d8154ae
Marco Menapace <m.menapace@fadeout.it>
2025-04-14 13:25:03 +0200

    [WIP] json as argument of launcher

--- s3m-runner/launcher.sh
+++ s3m-runner/launcher.sh
@@ -3,6 +3,7 @@ source $PYTHON_ENV_FILE
 
 export PYTHONPATH="${PYTHONPATH}:/app/shybox"
 
-pip install tabulate
+path_json=$1
+echo "path_json = " ${path_json}
 
-python /app/shybox/workflow/runner/app_runner_workflow_s3m_base_main.py -settings /app/shybox/workflow/runner/app_runner_workflow_s3m_base.json
+python /app/shybox/workflow/runner/app_runner_workflow_s3m_base_main.py -settings ${path_json}
