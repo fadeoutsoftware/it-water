6548ecfa840ca650c75a94c69357113242e161b7
Marco Menapace <m.menapace@fadeout.it>
2025-04-14 17:24:50 +0200

    dos2unix venvSetup S3M

--- s3m-runner/build.sh
+++ s3m-runner/build.sh
@@ -1,6 +1,7 @@
 ## DOS2UNIX launcher.sh
 dos2unix ./launcher.sh
 dos2unix ./app_runner_workflow_s3m_base.json
+dos2unix ./venvSetup.sh
 
 # Build docker image
 docker build --progress=plain -t it-water/s3m-runner:dev .
