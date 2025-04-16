dcd72e00f9c77975c0423c520d8c0cca5da8209f
Marco Menapace <m.menapace@fadeout.it>
2025-04-16 17:12:18 +0200

    Completed script and example for parametrization of orchestrator

--- script/orchestratorParameters/script.sh
+++ script/orchestratorParameters/script.sh
@@ -1,6 +1,25 @@
 config=./example_parameters.txt
-## cat + Sed to skip first line (the header)
-isRestart=$(cat example_parameters.txt|sed '1d'|awk '{print $1}')
-timeRestart=$(cat example_parameters.txt|sed '1d'|awk '{print $2}')
-timeStart=$(cat example_parameters.txt|sed '1d'|awk '{print $3}')
-timeEnd=$(cat example_parameters.txt|sed '1d'|awk '{print $4}')
+
+## Reference file for the extraction of parameters for the orchestrator from a 
+## specifically generated file.
+
+## Note: cat + Sed to skip first line (the header)
+rowCount=$(wc -l < ${config})
+for (( i = 1; i < $rowCount; i++ )) 
+do
+    isRestart=$(cat ${config}|sed '1d'|awk -v var1=${i} 'NR==var1{print $1}')
+    timeRestart=$(cat ${config}|sed '1d'|awk -v var1=${i} 'NR==var1{print $2}')
+    timeStart=$(cat ${config}|sed '1d'|awk -v var1=${i} 'NR==var1{print $3}')
+    timeEnd=$(cat ${config}|sed '1d'|awk -v var1=${i} 'NR==var1{print $4}')
+    timePeriod=$(cat ${config}|sed '1d'|awk -v var1=${i} 'NR==var1{print $5}')
+    slurmTimeLimit=$(cat ${config}|sed '1d'|awk -v var1=${i} 'NR==var1{print $6}')
+
+
+    echo "Launching with " 
+    echo "Restart mode " ${isRestart}
+    echo "Restart Time " ${timeRestart}
+    echo "Start Time " ${timeStart}
+    echo "End Time " ${timeEnd}
+    echo "Time Period " ${timePeriod}
+    echo "Slurm Time limit " ${slurmTimeLimit}
+done
