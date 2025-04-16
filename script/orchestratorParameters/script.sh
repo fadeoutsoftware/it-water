468c56eaf42c77e20529199f40f08b58244c04fe
Marco Menapace <m.menapace@fadeout.it>
2025-04-16 15:31:05 +0200

    comment

--- script/orchestratorParameters/script.sh
+++ script/orchestratorParameters/script.sh
@@ -1,4 +1,5 @@
 config=./example_parameters.txt
+## cat + Sed to skip first line (the header)
 isRestart=$(cat example_parameters.txt|sed '1d'|awk '{print $1}')
 timeRestart=$(cat example_parameters.txt|sed '1d'|awk '{print $2}')
 timeStart=$(cat example_parameters.txt|sed '1d'|awk '{print $3}')
