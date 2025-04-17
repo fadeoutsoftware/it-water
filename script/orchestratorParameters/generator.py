43b0126933bee3c89db96d2a796476b3305ef2cb
Marco Menapace <m.menapace@fadeout.it>
2025-04-17 16:18:53 +0200

    [WIP] generator ok, some cleaning required

--- script/orchestratorParameters/generator.py
+++ script/orchestratorParameters/generator.py
@@ -1,13 +1,69 @@
 import calendar
+from datetime import datetime,timedelta
+from dateutil.relativedelta import relativedelta
 
 def generateString (iRestart, oTimeRestart, oTimeStart, oTimeEnd, oTimePeriod, oSlurmTimeLimit, iS3MTerData):
     return iRestart + "          " + oTimeRestart + "    "+ oTimeStart + "  " + oTimeEnd +"  " + oTimePeriod + "            " + oSlurmTimeLimit + "        " + iS3MTerData
 
 ### DATE AND TIME TO START WITH 
-### DELTA T PER RIGA 
-### QUANTE RUN
+### DELTA T PER RICAVARE RANGE DI OGNI RIGA/SIMULAZIONE
 ### OCCHIO ORA ALLE DATE RESTART START END
-### RESTART SEMPRE 23:00 giorno prima DELLO START
+### RESTART SEMPRE 23:00 giorno prima DELLO START del primo giorno 
+
+def generateFromTo (startTime, endTime, stepMonths):
+    TIME_LIMIT = "0-02:00:01"
+    DATE_FORMAT = "%Y-%m-%d %H:%M"
+    with open('test.txt',"w", encoding="utf-8") as f:
+        f.write("isRestart  TimeRestart            TimeStart            TimeEnd              TimePeriod       SlurmTimeLimit    S3MTerData\n")
+        oStart= datetime.strptime(startTime, DATE_FORMAT)
+        oEnd = datetime.strptime(endTime, DATE_FORMAT)
+        
+
+        print("start " + str(oStart) + " end "+ str(oEnd))
+        # '1981-01-01 00:00' does not match format '%y-%m-%d %H:%M'
+        restart = 1
+        for i in range(1,stepMonths-1,1):
+             
+            a = oStart + relativedelta(months=+i-1)
+            b = oStart + relativedelta(months=+i) 
+            f.write(generateString(str(restart),
+                                   str(a-relativedelta(hours=+1)), 
+                                   str(a),
+                                   str(b) ,
+                                   str((b-a).total_seconds()/3600),
+                                   TIME_LIMIT,
+                                   str(int(not restart))))
+            f.write("\n")
+            restart = 0 # JUST ONCE FOR EACH SIMULATION
+
+def generateFromToWhile (startTime, endTime, stepMonths,stepDays,stepHours):
+    TIME_LIMIT = "0-02:00:01" ## Need to be calculated
+    DATE_FORMAT = "%Y-%m-%d %H:%M"
+    with open('testWhile.txt',"w", encoding="utf-8") as f:
+        f.write("isRestart  TimeRestart            TimeStart            TimeEnd              TimePeriod       SlurmTimeLimit    S3MTerData\n")
+        oStart= datetime.strptime(startTime, DATE_FORMAT)
+        oEnd = datetime.strptime(endTime, DATE_FORMAT)
+        
+
+        print("start " + str(oStart) + " end "+ str(oEnd))
+        # '1981-01-01 00:00' does not match format '%y-%m-%d %H:%M'
+        restart = 1
+        cursorTime = oStart
+        increment = relativedelta(months=stepMonths,days=stepDays,hours=stepHours)
+        while ((oEnd - cursorTime).total_seconds() >= 0 ):
+            
+            a = cursorTime 
+            b = cursorTime + increment
+            f.write(generateString(str(restart),
+                                   str(a-relativedelta(hours=+1)), 
+                                   str(a),
+                                   str(b) ,
+                                   str((b-a).total_seconds()/3600),
+                                   TIME_LIMIT,
+                                   str(int(not restart))))
+            f.write("\n")
+            restart = 0 # JUST ONCE FOR EACH SIMULATION
+            cursorTime += increment
 
 def generateOneYear (iYear, monthOfRestart):
     TIME_LIMIT = "0-02:00:01"
@@ -35,7 +91,7 @@ def generateOneYear (iYear, monthOfRestart):
             monthOfRestart -=1
 
 def main():
-    generateOneYear(2024,1)
+    generateFromToWhile("2024-01-01 00:00","2026-01-15 00:00",1,15,12) 
     
 
 
