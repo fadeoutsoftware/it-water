360c4acc920e40aea770473ce699bf066824bfef
Marco Menapace <m.menapace@fadeout.it>
2025-06-05 17:52:39 +0200

    Merger domain script working

--- mergerDomain/build.sh
+++ mergerDomain/build.sh
@@ -1,1 +1,8 @@
-docker build --progress=plain -t it-water/merger:dev . 
+
+## DOS2UNIX launcher.sh
+dos2unix ./launcher.sh
+dos2unix ./app_merger_by_domain_workflow_s3m_base.json
+dos2unix ./app_merger_by_domain_workflow_s3m_base_main.py
+dos2unix ./venvSetup.sh
+
+docker build --progress=plain -t it-water/mergerdomain:dev . 
---
+++ script/mergerdomain.sh
@@ -0,0 +1,11 @@
+dos2unix .env-mergerdomain
+
+docker run -it \
+-u root \
+--name merger \
+--mount type=bind,source=./data/case_study_merger_domain,target=/app/exec/data \
+--mount type=bind,source=./data/case_study_merger_domain/logs,target=/app/exec/logs \
+--env-file .env-mergerdomain \
+docker.io/it-water/mergerdomain:dev
+
+docker rm merger
