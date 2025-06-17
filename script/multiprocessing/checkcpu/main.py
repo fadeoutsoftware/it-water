b64a053a2ea193a13f86982b2d6246ed6231fe7f
Marco Menapace <m.menapace@fadeout.it>
2025-06-17 12:06:56 +0200

    Parallel execution of merger domain, to be tested

--- script/multiprocessing/checkcpu/main.py
+++ script/multiprocessing/checkcpu/main.py
@@ -6,12 +6,40 @@ from multiprocessing import Pool
 
 import time
 import random
+from datetime import datetime, timedelta
+import os
 
-intervals = (["01/02/2020", "01/03/2020"],
-             ["01/03/2020", "01/04/2020"],
-             ["01/04/2020", "01/05/2020"],
-             ["01/05/2020", "01/06/2020"])
 
+def split_datetime_intervals(start_date: str, end_date: str, num_intervals: int):
+    """
+    Split the interval between start_date and end_date into num_intervals equally spaced datetime strings.
+    Args:
+        start_date (str): Start date in "%Y-%m-%d %H:%M" format.
+        end_date (str): End date in "%Y-%m-%d %H:%M" format.
+        num_intervals (int): Number of intervals (number of points will be num_intervals + 1).
+    Returns:
+        List[str]: List of datetime strings in "%Y-%m-%d %H:%M" format.
+    """
+
+    fmt = "%Y-%m-%d %H:%M"
+    dt_start = datetime.strptime(start_date, fmt)
+    dt_end = datetime.strptime(end_date, fmt)
+    if num_intervals < 1:
+        raise ValueError("num_intervals must be >= 1")
+    total_seconds = (dt_end - dt_start).total_seconds()
+    step = total_seconds / num_intervals
+    result = []
+    for i in range(num_intervals):
+        array = [] 
+        dstart = dt_start + timedelta(seconds=(i) * step)
+        dstart = dstart.replace(minute=0)
+        dend = dt_start + timedelta(seconds=(i+1) * step) 
+        dend = dend.replace(minute=0)
+        array.append(dstart.strftime(fmt))
+        array.append(dend.strftime(fmt))
+        result.append(array)
+    return result
+    
 
 def work_log(work_data):
     print(" Process starting on dates between %s and %s " %
@@ -21,10 +49,12 @@ def work_log(work_data):
           (work_data[0], work_data[1]))
 
 
-def pool_handler():
-    p = Pool(4)
+def pool_handler(intervals):
+    
+    p = Pool(os.cpu_count())
     p.map(work_log, intervals)
 
 
 if __name__ == '__main__':
-    pool_handler()
+    intervals = split_datetime_intervals ("2010-01-01 12:00","2020-01-10 12:00",50) ## 10 days in  10 intervals
+    pool_handler(intervals)
