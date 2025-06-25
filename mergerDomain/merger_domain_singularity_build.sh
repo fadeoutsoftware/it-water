eab8b55ca5cf4d69d2e61b4badb7a6bf3dc3b384
Marco Menapace <m.mmenapace@fadeout.it>
2025-06-25 07:52:32 +0000

    bulding dev image for singularity

--- mergerDomain/merger_domain_singularity_build.sh
+++ mergerDomain/merger_domain_singularity_build.sh
@@ -5,6 +5,6 @@ rm mergerdomain.tar
 # Build docker image
 docker build --no-cache --progress=plain -t it-water/mergerdomain:dev .
 # Save image in tar format 
-docker save it-water/mergerdomain -o mergerdomain.tar
+docker save it-water/mergerdomain:dev -o mergerdomain.tar
 # Convert to singularity 
 singularity build mergerdomain.sif docker-archive://mergerdomain.tar
