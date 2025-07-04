777febb563dd92379ddcd87ce48782b0d3096a6a
Marco Menapace <m.menapace@fadeout.it>
2025-07-04 14:55:12 +0200

    minor on local build files

--- mergerTime/build.sh
+++ mergerTime/build.sh
@@ -1,5 +1,6 @@
 ## DOS2UNIX launcher.sh
 dos2unix ./launcher.sh
+dos2unix ./Dockerfile
 dos2unix ./app_merger_by_domain_workflow_s3m_base.json
 dos2unix ./app_merger_by_domain_workflow_s3m_base_main_parallel.py
 dos2unix ./venvSetup.sh
--- mergerTime/build_serial.sh
+++ mergerTime/build_serial.sh
@@ -1,8 +1,8 @@
 
 ## DOS2UNIX launcher.sh
-dos2unix ./launcher.sh
+dos2unix ./launcher_serial.sh
 dos2unix ./app_merger_by_time_workflow_s3m_base.json
 dos2unix ./app_merger_by_time_workflow_s3m_base_main.py
 dos2unix ./venvSetup.sh
 
-docker build --no-cache --progress=plain -t it-water/mergertime_serial:dev -f Dockerfile_serial .
+docker build --progress=plain -t it-water/mergertime_serial:dev -f Dockerfile_serial .
