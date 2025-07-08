abb8a61ee2594efd5c97ecd8b76aa28c7fe8cf56
Marco Menapace <m.menapace@fadeout.it>
2025-07-08 10:28:22 +0200

    Error in data reference

--- script/mergertimeserial.sh
+++ script/mergertimeserial.sh
@@ -3,9 +3,9 @@ dos2unix .env-mergerdomain
 docker run -it \
 -u root \
 --name merger \
---mount type=bind,source=./data/case_study_merger_domain,target=/app/exec/data \
---mount type=bind,source=./data/case_study_merger_domain/logs,target=/app/exec/logs \
---env-file .env-mergerdomain \
+--mount type=bind,source=./data/case_study_merger_time,target=/app/exec/data \
+--mount type=bind,source=./data/case_study_merger_time/logs,target=/app/exec/logs \
+--env-file .env-mergertime \
 docker.io/it-water/mergertime_serial:dev
 
 docker rm merger
