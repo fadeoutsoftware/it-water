a82f07987726c266789052f5127caf3a56c4ff2c
Marco Menapace <m.menapace@fadeout.it>
2025-08-08 17:35:54 +0200

    Support files for 1 thread 1 hour approach

---
+++ mergerDomain/build_hours.sh
@@ -0,0 +1,7 @@
+## DOS2UNIX launcher.sh
+dos2unix ./launcher.sh
+dos2unix ./app_merger_by_domain_workflow_s3m_base.json
+dos2unix ./app_merger_by_domain_workflow_s3m_base_main.py
+dos2unix ./venvSetup.sh
+
+docker build --progress=plain -t it-water/mergerdomain_hours:dev -f Dockerfile_hours .
