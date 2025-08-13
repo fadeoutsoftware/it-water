caf2651c3d2220d193ce9124b946ed83d330cf6b
Marco Menapace <m.menapace@fadeout.it>
2025-08-13 11:29:28 +0200

    singularity build relying on local docker image, instead of a complete rebuild

---
+++ s3m-converter/converter_singularity_build.sh
@@ -0,0 +1,15 @@
+
+#cleanup previous versions 
+rm converter.sif
+rm converter.tar
+## DOS2UNIX launcher.sh
+dos2unix ./launcher.sh
+dos2unix ./app_converter_workflow_s3m_base.json
+dos2unix ./app_converter_workflow_s3m_base_main.py
+
+# Build docker image
+docker build --no-cache --progress=plain -t it-water/converter:dev .
+# Save image in tar format 
+docker save it-water/converter:dev -o converter.tar
+# Convert to singularity 
+singularity build converter.sif docker-archive://converter.tar
