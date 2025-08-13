de49076aae0ee4d32952a200df06a51a5e3a4711
Marco Menapace <m.menapace@fadeout.it>
2025-08-13 17:01:57 +0200

    Build on top of local image

--- mergerDomain/merger_domain_singularity_build.sh
+++ mergerDomain/merger_domain_singularity_build.sh
@@ -3,8 +3,8 @@
 rm mergerdomain.sif
 rm mergerdomain.tar
 # Build docker image
-docker build --no-cache --progress=plain -t it-water/mergerdomain:dev .
+#docker build --no-cache --progress=plain -t it-water/mergerdomain:dev .
 # Save image in tar format 
-docker save it-water/mergerdomain:dev -o mergerdomain.tar
+docker save it-water/mergerdomain_hours:dev -o mergerdomain.tar
 # Convert to singularity 
 singularity build mergerdomain.sif docker-archive://mergerdomain.tar
