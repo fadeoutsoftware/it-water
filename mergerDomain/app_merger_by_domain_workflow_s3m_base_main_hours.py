01971692438af662b4ccecc8963823b705cd9ff4
Marco Menapace <m.menapace@fadeout.it>
2025-08-14 12:07:42 +0200

    Fixed approach with processes

--- mergerDomain/app_merger_by_domain_workflow_s3m_base_main_hours.py
+++ mergerDomain/app_merger_by_domain_workflow_s3m_base_main_hours.py
@@ -122,7 +122,7 @@ def split_datetime_24h(start_date: str, end_date: str):
     # 23 hours for each day of elaboration. The pool is fixed at 24, so the remaining following days are executed 
     # afterwards
     for j in range (nDays):
-        for i in range(23): 
+        for i in range(24): 
             array = [] 
             dstart = dt_start + timedelta(hours=(i)) + timedelta(days=j)
             
