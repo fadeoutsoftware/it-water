5d52be769ab3c7fc4dbb3a5d8851d911445012b5
Marco Menapace <m.menapace@fadeout.it>
2025-08-18 12:07:05 +0200

    fixed build script and created test in script folder

---
+++ script/hmc-converter.sh
@@ -0,0 +1,11 @@
+dos2unix .env-hmc-converter
+
+docker run -it \
+-u root \
+--name hmc-converter \
+--mount type=bind,source=./data/case_study_converter,target=/app/exec/data \
+--mount type=bind,source=./data/case_study_converter/logs,target=/app/exec/logs \
+--env-file .env-hmc-converter \
+docker.io/it-water/hmc-converter:dev
+
+docker rm hmc-converter
