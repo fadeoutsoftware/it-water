a04621c385e36dca700b5a40edd266099f3a3180
Marco Menapace <m.menapace@fadeout.it>
2025-08-18 12:08:18 +0200

    Singularity build script for hmc converter

--- hmc-converter/hmc_converter_singularity_build.sh
+++ hmc-converter/hmc_converter_singularity_build.sh
@@ -1,15 +1,15 @@
 
 #cleanup previous versions 
-rm converter.sif
-rm converter.tar
+rm hmc-converter.sif
+rm hmc-converter.tar
 ## DOS2UNIX launcher.sh
 dos2unix ./launcher.sh
 dos2unix ./app_converter_workflow_s3m_base.json
 dos2unix ./app_converter_workflow_s3m_base_main.py
 
 # Build docker image
-docker build --no-cache --progress=plain -t it-water/converter:dev .
+docker build --no-cache --progress=plain -t it-water/hmc-converter:dev .
 # Save image in tar format 
-docker save it-water/converter:dev -o converter.tar
+docker save it-water/hmc-converter:dev -o hmc-converter.tar
 # Convert to singularity 
-singularity build converter.sif docker-archive://converter.tar
+singularity build hmc-converter.sif docker-archive://hmc-converter.tar
