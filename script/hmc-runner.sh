d7fc974b739d74302916d83660669a096a4abbe9
Marco Menapace <m.menapace@fadeout.it>
2025-08-19 16:23:53 +0200

    Fixed env and runner for Marche domain example

--- script/hmc-runner.sh
+++ script/hmc-runner.sh
@@ -1,6 +1,8 @@
 docker run -it \
 --name hmc-runner \
 --mount type=bind,source=./data/case_study_hmc,target=/app/exec/data \
+--mount type=bind,source=./data/case_study_hmc/static/MarcheDomain,target=/app/exec/data/data_geo \
+--mount type=bind,source=./data/case_study_hmc/input,target=/app/exec/data/data_forcing/gridded \
 --env-file .env-hmc-runner \
 docker.io/it-water/hmc-runner:dev
 
