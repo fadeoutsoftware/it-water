94e2ce5c14a45a6ad540795d8482ffbb92ebb962
Marco Menapace <m.menapace@fadeout.it>
2025-08-19 16:24:21 +0200

    Improved build with dos2unix

--- hmc-runner/build.sh
+++ hmc-runner/build.sh
@@ -1,1 +1,8 @@
+## DOS2UNIX launcher.sh
+dos2unix ./launcher.sh
+dos2unix ./Dockerfile
+dos2unix ./app_runner_workflow_hmc_base.json
+dos2unix ./app_runner_workflow_hmc_base_main.py
+dos2unix ./venvSetup.sh
+
 docker build --progress=plain -t it-water/hmc-runner:dev .
