76727464586d3fa2a2f87be66d90684beff00674
Marco Menapace <m.menapace@fadeout.it>
2025-08-26 17:01:27 +0200

    Forcing fixed

--- hmc-converter/build.sh
+++ hmc-converter/build.sh
@@ -4,4 +4,4 @@ dos2unix ./launcher.sh
 dos2unix ./app_converter_workflow_hmc_base_main.py
 dos2unix ./app_converter_workflow_hmc_base.json
 
-docker build --progress=plain -t it-water/hmc-converter:dev . 
+docker build --progress=plain -t it-water/hmc-converter-forcing:dev . 
--- hmc-converter/hmc_converter_singularity_build.sh
+++ hmc-converter/hmc_converter_singularity_build.sh
@@ -1,15 +1,15 @@
 
 #cleanup previous versions 
-rm hmc-converter.sif
-rm hmc-converter.tar
+rm hmc-converter-forcing.sif
+rm hmc-converter-forcing.tar
 ## DOS2UNIX launcher.sh
 dos2unix ./launcher.sh
 dos2unix ./app_converter_workflow_s3m_base.json
 dos2unix ./app_converter_workflow_s3m_base_main.py
 
 # Build docker image
-docker build --no-cache --progress=plain -t it-water/hmc-converter:dev .
+docker build --no-cache --progress=plain -t it-water/hmc-converter-forcing:dev .
 # Save image in tar format 
-docker save it-water/hmc-converter:dev -o hmc-converter.tar
+docker save it-water/hmc-converter-forcing:dev -o hmc-converter-forcing.tar
 # Convert to singularity 
-singularity build hmc-converter.sif docker-archive://hmc-converter.tar
+singularity build hmc-converter-forcing.sif docker-archive://hmc-converter-forcing.tar
