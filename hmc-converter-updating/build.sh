a9907ebdec5adf5f9e50e01d986bf539d5a18fbe
Marco Menapace <m.menapace@fadeout.it>
2025-08-26 17:24:01 +0200

    also for this one

--- hmc-converter-updating/build.sh
+++ hmc-converter-updating/build.sh
@@ -4,4 +4,4 @@ dos2unix ./launcher.sh
 dos2unix ./app_converter_workflow_hmc_base_main.py
 dos2unix ./app_converter_workflow_hmc_base.json
 
-docker build --progress=plain -t it-water/hmc-converter-updating:dev . 
+docker build --no-cache --progress=plain -t it-water/hmc-converter-updating:dev . 
