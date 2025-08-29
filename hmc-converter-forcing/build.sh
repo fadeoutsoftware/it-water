e4506f6f3171a5aa53d37b3a1f536b9185ab20b4
Marco Menapace <m.menapace@fadeout.it>
2025-08-29 16:38:00 +0200

    Dos2unix to venv setup

--- hmc-converter-forcing/build.sh
+++ hmc-converter-forcing/build.sh
@@ -3,5 +3,6 @@
 dos2unix ./launcher.sh
 dos2unix ./app_converter_workflow_hmc_base_main.py
 dos2unix ./app_converter_workflow_hmc_base.json
+dos2unix ./venvSetup.sh
 
 docker build --no-cache --progress=plain -t it-water/hmc-converter-forcing:dev . 
