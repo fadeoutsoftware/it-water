9818ddf21ecad351bb5f6125a53866d7aeff8562
Marco Menapace <m.menapace@fadeout.it>
2026-01-19 16:34:49 +0100

    [WIP] Fixed missing import

--- hmc-converter-forcing/app_converter_workflow_hmc_base_main.py
+++ hmc-converter-forcing/app_converter_workflow_hmc_base_main.py
@@ -40,7 +40,7 @@ from multiprocessing import Process
 from shybox.config_toolkit.arguments_handler import ArgumentsManager
 from shybox.config_toolkit.config_handler import ConfigManager
 
-from shybox.orchestrator_toolkit.orchestrator_handler_base import OrchestratorHandler as Orchestrator
+from shybox.orchestrator_toolkit.orchestrator_handler_grid import OrchestratorGrid as Orchestrator
 from shybox.dataset_toolkit.dataset_handler_local import DataLocal
 from shybox.logging_toolkit.logging_handler import LoggingManager
 
