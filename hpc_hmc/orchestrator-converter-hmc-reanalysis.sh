5a5ff611a1c2943ef8b5c84f11a8340df3c77b89
Marco Menapace <m.menapace@fadeout.it>
2026-04-20 12:18:08 +0200

    Converter hmc aligned

--- hpc_hmc/orchestrator-converter-hmc-reanalysis.sh
+++ hpc_hmc/orchestrator-converter-hmc-reanalysis.sh
@@ -8,53 +8,49 @@ periods=$2
 # ----------------------------------------------------------------------------------------
 
 echo " ==================================================================================="
-echo " ==> Running HMC orchestrator for domains listed in "${domains}" with periods from "${periods}
+echo " ==> Running HMC converter orchestrator for domains listed in "${domains}" with periods from "${periods}
 
 iDomains=$(wc -l < ${domains})
 iPeriods=$(wc -l < ${periods})
-c_job_id=-1
-for (( j = 1; j < $iDomains; j++ ))
-do
-        domain_name=$(awk -v ArrayTaskID=$j '$1==ArrayTaskID {print $2}' $domains)
 
-        for (( i = 1; i < $iPeriods; i++ ))
-        do
+echo " Domains N "${iDomains}" "
+echo " Periods N "${iPeriods}" "
+c_f_job_id=-1
+c_u_job_id=-1
 
-                    time_start=$(cat ${periods}|sed '1d'|awk -v var1=${i} 'NR==var1{print $1}')
-                    time_end=$(cat ${periods}|sed '1d'|awk -v var1=${i} 'NR==var1{print $2}')
-                    time_period=$(cat ${periods}|sed '1d'|awk -v var1=${i} 'NR==var1{print $3}')
-                    days=$(cat ${periods}|sed '1d'|awk -v var1=${i} 'NR==var1{print $4}')
+for (( i = 1; i < $iPeriods; i++ ))
+          do
 
-                        calendar=${time_start:0:7}
-                        calendar=/g100_work/IscrC_ITWATER2/calendar/reanalysis/${calendar//-/}.time
+               time_start=$(cat ${periods}|sed '1d'|awk -v var1=${i} 'NR==var1{print $1}')
+               time_end=$(cat ${periods}|sed '1d'|awk -v var1=${i} 'NR==var1{print $2}')
+               time_period=$(cat ${periods}|sed '1d'|awk -v var1=${i} 'NR==var1{print $3}')
+               days=$(cat ${periods}|sed '1d'|awk -v var1=${i} 'NR==var1{print $4}')
 
-            echo " ==> Days "${days}" "
-            echo " ==> Calendar "${calendar}" "
-            echo " ==> Scheduling converter forcing reanalysis execution for domain: "${domain_name}"..."
+               echo " ==> Days "${days}" "
+               echo " ==> Scheduling converter forcing reanalysis execution for ALL domains"
+               
 
+               if [ $c_f_job_id == -1 ]; then
+                    c_f_job_id=$(sbatch --parsable --ntasks=${days} hmc-converter-forcing-reanalysis.slurm ${domains} ${time_start} ${time_end} ${days})
+               else
+                    c_f_job_id=$(sbatch --parsable --ntasks=${days} hmc-converter-forcing-reanalysis.slurm ${domains} ${time_start} ${time_end} ${days})
+               fi
+               
+               echo " ==> Converter reanalysis Forcing scheduled with job id "${c_f_job_id}" ."
 
-            if [ $c_job_id == -1 ]; then
-                 c_job_id=$(sbatch --parsable --ntasks=${days} hmc-converter-forcing-reanalysis.slurm ${domain_name} ${calendar})
-            else
-                 c_job_id=$(sbatch --parsable --ntasks=${days} --dependency=afterok:$c_job_id hmc-converter-forcing-reanalysis.slurm ${domain_name}  ${calendar})
-            fi
+               echo " ==> Scheduling converter Updating reanalysis execution for ALL domains"
+               if [ $c_u_job_id == -1 ]; then
+                    c_u_job_id=$(sbatch --parsable --ntasks=${days} hmc-converter-updating-reanalysis.slurm ${domains} ${time_start} ${time_end} ${days})
+               else
+                    c_u_job_id=$(sbatch --parsable --ntasks=${days} hmc-converter-updating-reanalysis.slurm ${domains} ${time_start} ${time_end} ${days})
+               fi
 
-
-            echo " ==> Converter reanalysis Forcing scheduled with job id "${c_job_id}" ."
-
-            echo " ==> Scheduling converter Updating reanalysis execution for domain: "${domain_name}" "${calendar}" ..."
-                        c_job_id=$(sbatch --parsable --ntasks=${days} --dependency=afterok:$c_job_id hmc-converter-updating-reanalysis.slurm ${domain_name} ${calendar})
-
-            echo " ==> Converter reanalysis updating scheduled with job id "${c_job_id}"."
-
-        done
-
+               echo " ==> Converter reanalysis updating scheduled with job id "${c_u_job_id}"."
+     
 done
 
 
 
 
-echo " ==> HMC orchestrator completed, jobs scheduled with SLURM"
+echo " ==> HMC converter orchestrator completed, jobs scheduled with SLURM"
 echo " ==================================================================================="
-
-
