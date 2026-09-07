296ab497eee46ab5f660caa4d1546ba6a1de5b74
Marco Menapace <m.menapace@fadeout.it>
2026-09-07 10:39:52 +0200

    Working configuration for final merger for Evapo Traspiration

--- mergerFinal/launcher.sh
+++ mergerFinal/launcher.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
-cd $CONDA_PATH 
-cd bin 
-source activate shybox_base_libraries
+cd /app/shybox
+source $CONDA_PATH/shybox_base_settings
 cd /app/shybox/workflow/merger
 export PYTHONPATH="${PYTHONPATH}:/app/shybox"
 
 python /app/shybox/workflow/merger/app_merger_by_domain_workflow_hmc_et.py -settings /app/shybox/workflow/merger/app_merger_by_domain_workflow_hmc_et.json
-<
